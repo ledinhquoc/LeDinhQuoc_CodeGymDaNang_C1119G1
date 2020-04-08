da60b13b265098737b28b0995145ec0b907e6409
acer123 <elvisle93@gmail.com>
2020-04-08 12:06:33 +0700

    'bt'

---
+++ Module3/angular-route-service/src/app/dictionary-detail/dictionary-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dictionary-detail',
+  templateUrl: './dictionary-detail.component.html',
+  styleUrls: ['./dictionary-detail.component.scss']
+})
+export class DictionaryDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
